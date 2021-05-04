635f1501de8981bb63bc367beb112a57c4cf266c
unknown <earlangelonadal@gmail.com>
2021-05-04 17:44:53 +0800

    add JWT authentication

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,16 @@ Route::delete('/like/{reply}','App\Http\Controllers\LikeController@unlikeIt');
 
 
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
