82c9242b6c646b818e20a6bc70f04e5f06fb05c6
unknown <earlangelonadal@gmail.com>
2021-05-05 11:54:45 +0800

    import vuetify, vuerouter, and initial components

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Toolbar from './Toolbar.vue'
+import AppFooter from './AppFooter.vue'
+export default {
+  components: { Toolbar,AppFooter },
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+
+      <v-toolbar-title>NadalE</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+     <v-toolbar-items>
+         <v-btn >
+             Forum
+         </v-btn>
+         <v-btn >
+             Ask Question
+         </v-btn>
+         <v-btn >
+             Category
+         </v-btn>
+         <v-btn >
+             Login
+         </v-btn>
+     </v-toolbar-items>
+      
+    </v-toolbar>
+  </v-card>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
